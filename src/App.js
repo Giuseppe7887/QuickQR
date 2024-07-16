ee6bf86e3815057ff80c8a0bd4df6b3eaaa77c4a
Giuseppe7887 <Giuseppegravante078@gmail.com>
2024-07-16 10:14:37 +0200

    gh-pages setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import QrPage from "./componenti/QrPage";
 
 
 
-export default function App(){
-  return(
-    <Router>
+export default function App() {
+  return (
+    <Router >
       <Routes>
-        <Route path="/" Component={Main}/>
-        <Route path="/qr" Component={QrPage}/>
+        <Route path="/" Component={Main} />
+        <Route path="/qr" Component={QrPage} />
+        <Route path="/QuickQR" Component={Main} />
+        <Route path="/QuickQR/qr" Component={QrPage} />
       </Routes>
     </Router>
   );
